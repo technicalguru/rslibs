8b8aa5f826e5d1b5d85b153a1747903b6678f0fe
Ralph Schuster <technicalguru@users.noreply.github.com>
2013-05-22 17:10:54 +0000

    Making it a module

---
+++ eu.ralph-schuster.data-hibernate/src/main/java/rs/data/hibernate/type/crypto/StringEncryptedType.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package rs.data.hibernate.type.crypto;
+
+import rsbaselib.io.ConverterUtils;
+import rsbaselib.util.CommonUtils;
+
+/**
+ * Handles Float encryption.
+ * @author ralph
+ *
+ */
+public class StringEncryptedType extends AbstractEncryptionType {
+
+	/**
+	 * Constructor.
+	 */
+	public StringEncryptedType() {
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Class<?> returnedClass() {
+		return String.class;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected Object convert(byte[] bytes) {
+		try {
+			return ConverterUtils.toString(bytes);
+		} catch (Exception e) {
+			throw new RuntimeException("Cannot convert to string: "+CommonUtils.toString(bytes), e);
+		}
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected byte[] convert(Object value) {
+		try {
+			return ConverterUtils.toBytes((String)value);
+		} catch (Exception e) {
+			throw new RuntimeException("Cannot convert string: "+value, e);
+		}
+	}
+
+}
