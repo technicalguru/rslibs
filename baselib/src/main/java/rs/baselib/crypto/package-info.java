42cba749c9abb1a8b764099905bc7d64836904f1
Ralph Schuster <technicalguru@users.noreply.github.com>
2013-06-23 08:54:20 +0000

    Moving tags for later reference

--- baselib/src/main/java/rs/baselib/crypto/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 	Provides support for encrypting, decrypting and signing streams.
- *	<p>
- *		The package provides standard implementations and utilities to
- *		easy encrypt and decrypt strings as well as working with signatures.
- *	</p>
- */
-package rs.baselib.crypto;
--- baselib/src/main/java/rs/baselib/io/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Provides I/O utilities, such as converting from/into bytes and finding resources.
- */
-package rs.baselib.io;
