791a1f80eece19f72f32893861850db2902a0e32
ralph <ralph@fdcf5c44-6d3c-11dc-8539-f79c8e7a170c>
2013-06-23 08:57:35 +0000

    Moving tags for later reference

---
+++ OSS/eu.ralph-schuster.libs/tags/libs-0.0.3/data-hibernate/src/main/java/rs/data/hibernate/type/crypto/TimestampEncryptedType.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package rs.data.hibernate.type.crypto;
+
+import java.sql.Timestamp;
+
+import rsbaselib.io.ConverterUtils;
+
+/**
+ * Handles Long encryption.
+ * @author ralph
+ *
+ */
+public class TimestampEncryptedType extends AbstractEncryptionType {
+
+	/**
+	 * Constructor.
+	 */
+	public TimestampEncryptedType() {
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Class<?> returnedClass() {
+		return Timestamp.class;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected Object convert(byte[] bytes) {
+		return ConverterUtils.toTimestamp(bytes);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected byte[] convert(Object value) {
+		return ConverterUtils.toBytes((Timestamp)value);
+	}
+
+}
