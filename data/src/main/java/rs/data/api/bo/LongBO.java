bde68552b5ea939f47e94ca9e4916f8919a84776
Ralph Schuster <technicalguru@users.noreply.github.com>
2013-06-23 08:55:47 +0000

    Moving tags for later reference

---
+++ data/src/main/java/rs/data/api/bo/LongBO.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package rs.data.api.bo;
+
+
+/**
+ * Business Object using {@link Long} as its key class.
+ * @param <T> type of Transfer Object
+ * @author ralph
+ *
+ */
+public interface LongBO extends GeneralBO<Long> {
+
+}
