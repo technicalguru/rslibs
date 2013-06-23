14adbdc49e911eb2b712799682e76a32a372b6bc
ralph <ralph@fdcf5c44-6d3c-11dc-8539-f79c8e7a170c>
2013-06-23 08:57:23 +0000

    Moving tags for later reference

---
+++ OSS/eu.ralph-schuster.libs/tags/libs-0.0.2/data-hibernate/src/main/java/rs/data/hibernate/type/crypto/ICryptingDelegateFactory.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package rs.data.hibernate.type.crypto;
+
+import java.security.KeyPair;
+import java.security.spec.AlgorithmParameterSpec;
+
+/**
+ * Interface for factories creating crypting delegates.
+ * @author ralph
+ *
+ */
+public interface ICryptingDelegateFactory {
+
+	/**
+	 * Returns a crypting delegate.
+	 * @return a delegate
+	 */
+	public ICryptingDelegate getCryptingDelegate();
+	
+	/**
+	 * Returns the key.
+	 * @return the key
+	 */
+	public KeyPair getKeyPair();
+
+	/**
+	 * Returns the algorithm.
+	 * @return the algorithm
+	 */
+	public String getAlgorithm();
+	
+	/**
+	 * Returns the paramSpec.
+	 * @return the paramSpec
+	 */
+	public AlgorithmParameterSpec getParamSpec();
+	
+	/**
+	 * Alternatively provide a passphrase for encryption usage.
+	 * @return passphrase
+	 */
+	public char[] getPassphrase();
+	
+	/**
+	 * Provide a salt for encryption usage.
+	 * @return salt
+	 */
+	public byte[] getSalt();
+}
