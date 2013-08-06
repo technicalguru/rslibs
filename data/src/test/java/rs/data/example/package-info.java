7a81e882f6352d4f08d5b0b6bddeeb7e5f8f6c39
Ralph Schuster <technicalguru@users.noreply.github.com>
2013-08-06 14:16:29 +0000

    RSLIBS-28 Add support for files

---
+++ data/src/test/java/rs/data/example/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * The example code as explained at 
+ * <a href="http://techblog.ralph-schuster.eu/rs-library/data-base-classes/usage-of-data-base-classes/">Website</a>.
+ * @author ralph
+ *
+ */
+package rs.data.example;
