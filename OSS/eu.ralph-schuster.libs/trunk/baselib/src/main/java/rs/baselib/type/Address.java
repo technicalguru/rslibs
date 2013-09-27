87415efb9b470d0f13060f7e372d89d468ce0d46
ralph <ralph@fdcf5c44-6d3c-11dc-8539-f79c8e7a170c>
2013-09-27 10:47:39 +0000

    RSLIBS-28 Support for file based data

---
+++ OSS/eu.ralph-schuster.libs/trunk/baselib/src/main/java/rs/baselib/type/Address.java
@@ -0,0 +1,161 @@
+/**
+ * 
+ */
+package rs.baselib.type;
+
+import java.io.Serializable;
+
+import rs.baselib.bean.AbstractBean;
+
+/**
+ * A location address.
+ * @author ralph
+ *
+ */
+public class Address extends AbstractBean implements Serializable {
+
+	/** Serial UID */
+	private static final long serialVersionUID = 1L;
+
+	/** Property name of street1 */
+	public static final String STREET1 = "street1";
+	/** Property name of street2 */
+	public static final String STREET2 = "street2";
+	/** Property name of city */
+	public static final String CITY = "city";
+	/** Property name of zipCode */
+	public static final String ZIP_CODE = "zipCode";
+	/** Property name of state */
+	public static final String STATE = "state";
+	/** Property name of country */
+	public static final String COUNTRY = "country";
+	
+	/** First line of street address */
+	private String street1;
+	/** Seconds line of street address */
+	private String street2;
+	/** Name of city */
+	private String city;
+	/** ZIP-Code of city */
+	private String zipCode;
+	/** State of city */
+	private String state;
+	/** Country */
+	private Country country;
+	
+	/**
+	 * Constructor.
+	 */
+	public Address() {
+	}
+
+	/**
+	 * Returns the {@link #street1}.
+	 * @return the street1
+	 */
+	public String getStreet1() {
+		return street1;
+	}
+
+	/**
+	 * Sets the {@link #street1}.
+	 * @param street1 the street1 to set
+	 */
+	public void setStreet1(String street1) {
+		String oldValue = getStreet1();
+		this.street1 = street1;
+		firePropertyChange(STREET1, oldValue, street1);
+	}
+
+	/**
+	 * Returns the {@link #street2}.
+	 * @return the street2
+	 */
+	public String getStreet2() {
+		return street2;
+	}
+
+	/**
+	 * Sets the {@link #street2}.
+	 * @param street2 the street2 to set
+	 */
+	public void setStreet2(String street2) {
+		String oldValue = getStreet2();
+		this.street2 = street2;
+		firePropertyChange(STREET2, oldValue, street2);
+	}
+
+	/**
+	 * Returns the {@link #city}.
+	 * @return the city
+	 */
+	public String getCity() {
+		return city;
+	}
+
+	/**
+	 * Sets the {@link #city}.
+	 * @param city the city to set
+	 */
+	public void setCity(String city) {
+		String oldValue = getCity();
+		this.city = city;
+		firePropertyChange(CITY, oldValue, city);
+	}
+
+	/**
+	 * Returns the {@link #zipCode}.
+	 * @return the zipCode
+	 */
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	/**
+	 * Sets the {@link #zipCode}.
+	 * @param zipCode the zipCode to set
+	 */
+	public void setZipCode(String zipCode) {
+		String oldValue = getZipCode();
+		this.zipCode = zipCode;
+		firePropertyChange(ZIP_CODE, oldValue, zipCode);
+	}
+
+	/**
+	 * Returns the {@link #state}.
+	 * @return the state
+	 */
+	public String getState() {
+		return state;
+	}
+
+	/**
+	 * Sets the {@link #state}.
+	 * @param state the state to set
+	 */
+	public void setState(String state) {
+		String oldValue = getState();
+		this.state = state;
+		firePropertyChange(STATE, oldValue, state);
+	}
+
+	/**
+	 * Returns the {@link #country}.
+	 * @return the country
+	 */
+	public Country getCountry() {
+		return country;
+	}
+
+	/**
+	 * Sets the {@link #country}.
+	 * @param country the country to set
+	 */
+	public void setCountry(Country country) {
+		Country oldValue = getCountry();
+		this.country = country;
+		firePropertyChange(COUNTRY, oldValue, country);
+	}
+
+	
+}
---
+++ OSS/eu.ralph-schuster.libs/trunk/baselib/src/main/java/rs/baselib/type/Continent.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package rs.baselib.type;
+
+/**
+ * All continents on this planet.
+ * @author ralph
+ *
+ */
+public enum Continent {
+
+	AFRICA,
+	ANTARCTICA,
+	ARCTICA,
+	ASIA,
+	AUSTRALIA,
+	EUROPE,
+	NORTH_AMERICA,
+	SOUTH_AMERICA;
+}
