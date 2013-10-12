4c67b17ed2cd87b0513b22a8e2aed85941256591
ralph <ralph@fdcf5c44-6d3c-11dc-8539-f79c8e7a170c>
2013-10-12 12:12:58 +0000

    RSLIBS-33 Expose debugging options for programmatic access

--- OSS/eu.ralph-schuster.libs/trunk/data/src/main/java/rs/data/JotmSupport.java
+++ OSS/eu.ralph-schuster.libs/trunk/data/src/main/java/rs/data/JotmSupport.java
@@ -11,6 +11,8 @@ import org.objectweb.jotm.Jotm;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import rs.baselib.lang.LangUtils;
+
 /**
  * Involves transactions.
  * @author ralph
@@ -22,11 +24,15 @@ public class JotmSupport {
 	
 	private static Jotm jotm;
 	private static TransactionManager txManager;
+	private static boolean debugTransactions = false;
+	private static boolean traceTransactions = false;
 	
 	/**
 	 * Starts transaction support.
 	 */
 	public static synchronized void start() throws NamingException {
+		setDebugTransactions(LangUtils.getBoolean(System.getProperty("transaction.debug")));
+		setTraceTransactions(LangUtils.getBoolean(System.getProperty("transaction.trace")));
 		if (txManager == null) {
 			jotm = new Jotm(true, false);
 			setTransactionManager(jotm.getTransactionManager());
@@ -67,6 +73,42 @@ public class JotmSupport {
 	 * @return the user transaction
 	 */
 	public static UserTransaction getUserTransaction() {
+		log.debug("TX-"+Thread.currentThread().getId()+" returned");
 		return jotm.getUserTransaction();
 	}
+	
+	/**
+	 * Returns whether debugging of transaction demarcations.
+	 * @return <code>true</code> when debugging shall be enabled (via SLF4J)
+	 */
+	public static boolean isDebugTransactions() {
+		return debugTransactions;
+	}
+	
+	/**
+	 * Sets debugging of transaction demarcations.
+	 * @param debug <code>true</code> when debugging shall be enabled (via SLF4J)
+	 */
+	public static void setDebugTransactions(boolean debug) {
+		debugTransactions = debug;
+		if (debugTransactions) log.info("Enabling transaction demarcation log");
+	}
+	
+	/**
+	 * Returns whether stacktracing is enabled with {@link #isDebugTransactions() transaction demarcation debugging} option.
+	 * @return <code>true</code> when stacktrace shall be enabled (via SLF4J)
+	 */
+	public static boolean isTraceTransactions() {
+		return traceTransactions;
+	}
+	
+	/**
+	 * Sets whether stacktracing is enabled with {@link #isDebugTransactions() transaction demarcation debugging} option.
+	 * @param trace <code>true</code> when stacktrace shall be enabled (via SLF4J)
+	 */
+	public static void setTraceTransactions(boolean trace) {
+		traceTransactions = trace;
+		if (debugTransactions && traceTransactions) log.info("Enabling stacktrace for transaction demarcation");
+	}
+
 }
