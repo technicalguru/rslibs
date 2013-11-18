9368792f26451c1a65ff4c70cb364fe7d4cd0415
ralph <ralph@fdcf5c44-6d3c-11dc-8539-f79c8e7a170c>
2013-11-18 15:44:11 +0000

    RSLIBS-47 URI/URL Provider interfaces

---
+++ OSS/eu.ralph-schuster.libs/trunk/baselib/src/main/java/rs/baselib/util/IUriProvider.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package rs.baselib.util;
+
+import java.net.URI;
+
+/**
+ * Interface that can provide URIs.
+ * @author ralph
+ *
+ */
+public interface IUriProvider {
+
+	/**
+	 * Returns the URL.
+	 * @return the URL
+	 */
+	public URI getUri();
+}
---
+++ OSS/eu.ralph-schuster.libs/trunk/baselib/src/main/java/rs/baselib/util/IUrlProvider.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package rs.baselib.util;
+
+import java.net.URL;
+
+/**
+ * Interface that can provide URLs.
+ * @author ralph
+ *
+ */
+public interface IUrlProvider {
+
+	/**
+	 * Returns the URL.
+	 * @return the URL
+	 */
+	public URL getUrl();
+}
