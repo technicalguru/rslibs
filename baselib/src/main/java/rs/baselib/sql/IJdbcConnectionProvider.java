809bd353f6d9d6a45761e16e44ba39977a99d7ca
Ralph Schuster <technicalguru@users.noreply.github.com>
2013-11-21 10:27:43 +0000

    RSLIBS-50 Connection providers must return URL

--- baselib/src/main/java/rs/baselib/sql/IJdbcConnectionProvider.java
+++ baselib/src/main/java/rs/baselib/sql/IJdbcConnectionProvider.java
@@ -33,4 +33,17 @@ public interface IJdbcConnectionProvider {
 	 * @throws SQLException when the connection cannot be opened
 	 */
 	public Connection getConnection(String host, String port, String dbName, String dbLogin, String dbPassword, String... addOnArgs) throws SQLException;
+
+	/**
+	 * Constructs the driver's URL from the arguments.
+	 * The method shall return <code>null</code> in case of any problems or throw an exception.
+	 * @param host the host of the database
+	 * @param port the port of the database (some DB use names as port information)
+	 * @param dbName name of database
+	 * @param dbLogin database login
+	 * @param dbPassword database password
+	 * @param addOnArgs additional arguments for the driver URL
+	 * @return the URL for the driver or <code>null</code>
+	 */
+	public String getDriverUrl(String host, String port, String dbName, String dbLogin, String dbPassword, String... addOnArgs);
 }
