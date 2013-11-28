b76819563f852c89e379b39b62be68127ae0504a
Ralph Schuster <technicalguru@users.noreply.github.com>
2013-11-28 16:56:48 +0000

    RSBUDGET-1 Licensing made usable for apps

--- baselib/src/main/java/rs/baselib/licensing/LicenseManager.java
+++ baselib/src/main/java/rs/baselib/licensing/LicenseManager.java
@@ -3,7 +3,14 @@
  */
 package rs.baselib.licensing;
 
+import java.io.File;
+import java.security.KeyFactory;
 import java.security.PublicKey;
+import java.security.spec.X509EncodedKeySpec;
+
+import org.apache.commons.codec.binary.Base64;
+
+import rs.baselib.util.CommonUtils;
 
 /**
  * The Key Manager, responsible to verify licenses.
@@ -13,14 +20,14 @@ import java.security.PublicKey;
 public class LicenseManager {
 
 	private ILicenseVerifier licenseVerifier;
-	
+
 	/**
 	 * Constructor.
 	 */
 	public LicenseManager(PublicKey publicKey) {
 		this(publicKey, null);
 	}
-	
+
 	/**
 	 * Constructor.
 	 */
@@ -28,14 +35,14 @@ public class LicenseManager {
 		this(scheme != null ? scheme.getLicenseVerifier() : LicensingScheme.FULL_LICENSE.getLicenseVerifier());
 		getLicenseVerifier().setKey(publicKey);
 	}
-	
+
 	/**
 	 * Constructor.
 	 */
 	public LicenseManager(ILicenseVerifier licenseVerifier) {
 		this.licenseVerifier = licenseVerifier;
 	}
-	
+
 	/**
 	 * Returns the {@link #licenseVerifier}.
 	 * @return the licenseVerifier
@@ -61,4 +68,37 @@ public class LicenseManager {
 		}
 	}
 
+	/**
+	 * Instantiates a {@link LicenseManager} from given file with public key.
+	 * @param f file where public key is stored in BASE64 X.509 encoding 
+	 * @param algorithm algorithm that the key used
+	 * @param scheme the licensing scheme
+	 * @return the manager
+	 * @throws LicenseException when the manager cannot be instantiated
+	 */
+	public static LicenseManager generateFrom(File f, String algorithm) {
+		return generateFrom(f, algorithm, LicensingScheme.FULL_LICENSE);
+	}
+	
+	/**
+	 * Instantiates a {@link LicenseManager} from given file with public key.
+	 * @param f file where public key is stored in BASE64 X.509 encoding 
+	 * @param algorithm algorithm that the key used
+	 * @param scheme the licensing scheme
+	 * @return the manager
+	 * @throws LicenseException when the manager cannot be instantiated
+	 */
+	public static LicenseManager generateFrom(File f, String algorithm, ILicensingScheme scheme) {
+		try {
+			byte[] encodedPublicKey = Base64.decodeBase64(CommonUtils.loadContent(f));
+			KeyFactory keyFactory = KeyFactory.getInstance(algorithm);
+			X509EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(encodedPublicKey);
+			PublicKey publicKey = keyFactory.generatePublic(publicKeySpec);
+			return new LicenseManager(publicKey, scheme);
+		} catch (LicenseException e) {
+			throw e;
+		} catch (Throwable t) {
+			throw new LicenseException("Cannot create LicenseManager", t);
+		}
+	}
 }
