0d9502160f4159fb6a696e1b4ce8df9bc1186316
ralph <ralph@fdcf5c44-6d3c-11dc-8539-f79c8e7a170c>
2014-05-04 11:20:58 +0000

    RSLIBS-108 Removing superfluous check

--- OSS/eu.ralph-schuster.libs/trunk/baselib/src/main/java/rs/baselib/io/XmlReaderFilter.java
+++ OSS/eu.ralph-schuster.libs/trunk/baselib/src/main/java/rs/baselib/io/XmlReaderFilter.java
@@ -42,8 +42,7 @@ public class XmlReaderFilter extends AbstractReaderFilter {
 	public boolean isValidChar(char c) {
 		return (c == 0x9) ||  (c == 0xA) ||  (c == 0xD) || 
 				((c >= 0x20) && (c <= 0xD7FF)) || 
-				((c >= 0xE000) && (c <= 0xFFFD)) || 
-				((c >= 0x10000) && (c <= 0x10FFFF));
+				((c >= 0xE000) && (c <= 0xFFFD));
 	}
 
 }
