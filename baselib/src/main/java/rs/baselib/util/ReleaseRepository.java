becfed637a42fe377712a36e52c736a4005f127d
Ralph Schuster <technicalguru@users.noreply.github.com>
2014-05-05 09:38:20 +0000

    RSLIBS-121 not catching RuntimeExceptions

--- baselib/src/main/java/rs/baselib/util/ReleaseRepository.java
+++ baselib/src/main/java/rs/baselib/util/ReleaseRepository.java
@@ -117,6 +117,8 @@ public class ReleaseRepository {
 					added = addPomProperties(new URL(desc.getUrlPrefix()+entry.getName()));
 				}
 			}
+		} catch (RuntimeException e) {
+			throw e;
 		} catch (Exception e) {
 			// Ignore: no such info
 		}
