5cb15f95858126e0cfab766050a5cbf2ec0681d9
Ralph Schuster <ralph.schuster@dlh.de>
2023-02-16 13:47:58 +0100

    Add parsing methods for ZonedDateTime

--- baselib/src/main/java/rs/baselib/util/DateTimeUtils.java
+++ baselib/src/main/java/rs/baselib/util/DateTimeUtils.java
@@ -1,5 +1,9 @@
 package rs.baselib.util;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
 import java.time.temporal.TemporalAmount;
@@ -96,5 +100,70 @@ public class DateTimeUtils {
 		return time.withHour(23).withMinute(59).withSecond(59).withNano(0);
 	}
 	
+	/**
+	 * Tries to parse the string in order to retrieve a date/time with default ZoneId and default time 00:00:00.
+	 * @param s - the string to be parsed
+	 * @return the {@link ZonedDateTime} object or {@code null} if not successful.
+	 */
+	public static  ZonedDateTime parseZonedDateTime(String s) {
+		return parseZonedDateTime(s, ZoneId.systemDefault());
+	}
+	
+	/**
+	 * Tries to parse the string in order to retrieve a date/time default time 00:00:00.
+	 * @param s - the string to be parsed
+	 * @param defaultZone - default zone ID to be used
+	 * @return the {@link ZonedDateTime} object or {@code null} if not successful.
+	 */
+	public static  ZonedDateTime parseZonedDateTime(String s, ZoneId defaultZone) {
+		return parseZonedDateTime(s, LocalTime.of(0, 0, 0), defaultZone);
+	}
+	
+	/**
+	 * Tries to parse the string in order to retrieve a date/time with default ZoneId and default time 23:59:59.999999999.
+	 * @param s - the string to be parsed
+	 * @return the {@link ZonedDateTime} object or {@code null} if not successful.
+	 */
+	public static  ZonedDateTime parseZonedDateTimeWithEndOfDay(String s) {
+		return parseZonedDateTimeWithEndOfDay(s, ZoneId.systemDefault());
+	}
+	
+	/**
+	 * Tries to parse the string in order to retrieve a date/time with default time 23:59:59.999999999.
+	 * @param s - the string to be parsed
+	 * @param defaultZone - default zone ID to be used
+	 * @return the {@link ZonedDateTime} object or {@code null} if not successful.
+	 */
+	public static  ZonedDateTime parseZonedDateTimeWithEndOfDay(String s, ZoneId defaultZone) {
+		return parseZonedDateTime(s, LocalTime.of(23, 59, 59, 999999999), defaultZone);
+	}
+	
+	/**
+	 * Tries to parse the string in order to retrieve a date/time with default ZoneId and given default time.
+	 * @param s - the string to be parsed
+	 * @param defaultTime - default time to be used
+	 * @return the {@link ZonedDateTime} object or {@code null} if not successful.
+	 */
+	public static  ZonedDateTime parseZonedDateTime(String s, LocalTime defaultTime) {
+		return parseZonedDateTime(s, defaultTime, ZoneId.systemDefault());
+	}
+	
+	/**
+	 * Tries to parse the string in order to retrieve a date/time with default ZoneId and given default time and zoneId.
+	 * @param s - the string to be parsed
+	 * @param defaultTime - default time to be used
+	 * @param defaultZone - default zone ID to be used
+	 * @return the {@link ZonedDateTime} object or {@code null} if not successful.
+	 */
+	public static  ZonedDateTime parseZonedDateTime(String s, LocalTime defaultTime, ZoneId defaultZone) {
+		if (CommonUtils.isEmpty(s)) return null;
+		s = CommonUtils.trim(s);
+		// try a few things
+		try { return ZonedDateTime.parse(s); } catch (Throwable t) {}
+		try { return ZonedDateTime.of(LocalDateTime.parse(s), defaultZone ); } catch (Throwable t) {}
+		try { return ZonedDateTime.of(LocalDate.parse(s), defaultTime, defaultZone ); } catch (Throwable t) {}
+		return null; 
+	}
+
 
 }
